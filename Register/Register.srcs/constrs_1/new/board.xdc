485925899e2fee4abbb183d528c288a26b60cfaa
syk <wysyk2018@gmail.com>
2019-04-18 12:52:46 +0800

    1

--- Register/Register.srcs/constrs_1/new/board.xdc
+++ Register/Register.srcs/constrs_1/new/board.xdc
@@ -97,15 +97,3 @@ set_property PACKAGE_PIN M21 [get_ports {which[1]}]
 set_property PACKAGE_PIN M22 [get_ports {which[2]}]
 set_property -dict {IOSTANDARD LVCMOS18 PACKAGE_PIN L21} [get_ports enable]
 set_property -dict {IOSTANDARD LVCMOS18 PACKAGE_PIN H4} [get_ports clk]
-
-# [Place 30-574] Poor placement for routing between an IO pin and BUFG.If this
-# sub optimal condition is acceptable for this design, you may use the
-# CLOCK_DEDICATED_ROUTE constraint in the .xdc file to demote this message to a
-# WARNING. However, the use of this override is highly discouraged.
-set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets clk_IBUF]
-set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets swb_IBUF[1]]
-set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets swb_IBUF[2]]
-set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets swb_IBUF[3]]
-set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets swb_IBUF[4]]
-set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets swb_IBUF[5]]
-set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets swb_IBUF[6]]
--- Register/Register.srcs/sources_1/new/board.v
+++ Register/Register.srcs/sources_1/new/board.v
@@ -24,24 +24,23 @@ module board(
     input[31:0] myInput,
     input[5:0] swb,
     input clk,
+    output[2:0] which,
+    output[7:0] seg,
     output[31:0] led,
-    output [2:0] which,
-    output [7:0] seg,
     output reg enable = 1
     );
     
     reg Write_Reg = 0;
-    reg flag = 0;
     reg[4:0] R_Addr_A,R_Addr_B,W_Addr;
-    reg[31:0] led_result,dis_result,W_Data;
+    reg[31:0] Dis_result,Led_result,W_Data;
     wire[31:0] R_Data_A,R_Data_B;
     
-    assign led = led_result;
+    assign led = Led_result;
     //按钮1：输入32位前15位为寄存器地址
-    //按钮2：输入32位为输入寄存器的内容
+    //按钮2：输入32位为s输入寄存器的内容
     //按钮3：最高位开关为写信号，1时执行写操作
     //按钮4：A/B读端口的选择
-    //按钮5：清零
+    //按钮5：清零 
     //按钮6：模拟clk信号
     assign toggle = swb[0]|swb[1]|swb[2]|swb[3];
     
@@ -58,12 +57,12 @@ module board(
                 6'b000100:Write_Reg <= myInput[31];
                 6'b001000:
                     begin
-                        led_result <= R_Data_A;
-                        dis_result <= R_Data_B;        
+                       Led_result <= R_Data_A;
+                       Dis_result <= R_Data_B;         
                     end
             endcase    
         end
     register myregister(R_Addr_A,R_Addr_B,W_Addr,Write_Reg,swb[4],swb[5],W_Data,R_Data_A,R_Data_B);
-    Display Display_Instance(.clk(clk), .data(dis_result),
+    Display Display_Instance(.clk(clk), .data(Dis_result),
         .which(which), .seg(seg));
 endmodule

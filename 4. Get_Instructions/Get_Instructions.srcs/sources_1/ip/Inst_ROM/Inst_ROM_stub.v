808431fb28163f7cfc8d652194c18b3ce88701ae
syk2018 <46128472+syk2018@users.noreply.github.com>
2019-05-30 16:13:57 +0800

    更新存储器、取指令模块

--- 4. Get_Instructions/Get_Instructions.srcs/sources_1/ip/Inst_ROM/Inst_ROM_stub.v
+++ 4. Get_Instructions/Get_Instructions.srcs/sources_1/ip/Inst_ROM/Inst_ROM_stub.v
@@ -1,10 +1,10 @@
 // Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
-// Date        : Thu May 23 15:30:31 2019
+// Date        : Thu May 30 10:16:29 2019
 // Host        : DESKTOP-B8MAG06 running 64-bit major release  (build 9200)
-// Command     : write_verilog -force -mode synth_stub
-//               c:/GitHub/PCC-HomeWork/Get_Instructions/Get_Instructions.srcs/sources_1/ip/Inst_ROM/Inst_ROM_stub.v
+// Command     : write_verilog -force -mode synth_stub {C:/GitHub/PCC-HomeWork/4.
+//               Get_Instructions/Get_Instructions.srcs/sources_1/ip/Inst_ROM/Inst_ROM_stub.v}
 // Design      : Inst_ROM
 // Purpose     : Stub declaration of top-level module interface
 // Device      : xc7a100tfgg484-2L
